31007d18343bfef1a859d4488d07773da7f1510b
zengqi <qi.zeng1203@gmail.com>
2022-10-08 23:30:27 -0700

    Q7 done

---
+++ Assignment2/Q7/src/Main.java
@@ -0,0 +1,40 @@
+public class Main {
+
+    public static void main(String[] args) {
+	//Q7 Lowest Common Ancestor of a Binary Tree
+    }
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null || root == p || root == q) {
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left == null && right == null) {
+            return null;
+        }
+
+        if(left ==null) {
+            return right;
+        }
+
+        if(right == null) {
+            return left;
+        }
+        return root;
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
