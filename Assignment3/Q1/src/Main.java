0e722142cfe2f66026f51ae2fe414a0044feb2eb
zengqi <qi.zeng1203@gmail.com>
2022-10-18 19:18:24 -0700

    assignment 3 Q1

--- Assignment3/Q1/src/Main.java
+++ Assignment3/Q1/src/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-	// write your code here
+	// Q1 Combination Sum
         int[] array = {2,3,6,7};
         int target = 7;
         System.out.println(combinationSum(array, target));
