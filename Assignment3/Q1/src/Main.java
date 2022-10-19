0c4503ac57082a7c56881304314b55b1159c8326
zengqi <qi.zeng1203@gmail.com>
2022-10-18 18:08:50 -0700

    assignment 3 Q1

---
+++ Assignment3/Q1/src/Main.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        int[] array = {2,3,6,7};
+        int target = 7;
+        System.out.println(combinationSum(array, target));
+    }
+
+    static List<List<Integer>> result = new ArrayList<>();
+    static LinkedList<Integer> path = new LinkedList<>();
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Arrays.sort(candidates);
+        backTracking(candidates, target,0,0);
+        return result;
+    }
+    public static void backTracking(int[] candidates, int target, int sum, int startIndex) {
+        if(sum > target) {
+            return;
+        }
+        if(sum == target) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        for(int i = startIndex; i < candidates.length; i++) {
+            sum += candidates[i];
+            path.add(candidates[i]);
+            backTracking(candidates, target, sum, i);
+            sum -= candidates[i];
+            path.removeLast();
+        }
+    }
+}
