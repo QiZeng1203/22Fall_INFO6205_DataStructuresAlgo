1a3cde801316accae5998f13548facb7312ee920
zengqi <qi.zeng1203@gmail.com>
2022-10-20 21:18:32 -0700

    Q8

---
+++ Assignment4/Q8/src/Main.java
@@ -0,0 +1,33 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if(root == null) {
+            return 0;
+        }
+        if(root.val < low) {
+            return rangeSumBST(root.right, low, high);
+        }
+        if(root.val > high) {
+            return rangeSumBST(root.left, low, high);
+        }
+
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
