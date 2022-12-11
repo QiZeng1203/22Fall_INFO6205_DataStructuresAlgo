44303cdcb1fc60694ddfd65f8449144aa87185a7
zengqi <qi.zeng1203@gmail.com>
2022-12-11 11:40:38 -0800

    Final Q1

--- Final/Q1/src/Main.java
+++ Final/Q1/src/Main.java
@@ -10,31 +10,31 @@ public class Main {
     // Time Complexity: O(n)
 
     public static List<List<String>> groupAnagrams(String[] strs) {
-            List<List<String>> list = new ArrayList<>();
+        List<List<String>> list = new ArrayList<>();
 
-            HashMap<String, ArrayList<String>> map = new HashMap<>();
+        HashMap<String, ArrayList<String>> map = new HashMap<>();
 
-            for(String str: strs) {
-                char[] chArr = str.toCharArray();
-                Arrays.sort(chArr);
-                String anagram = String.valueOf(chArr);
+        for(String str: strs) {
+            char[] chArr = str.toCharArray();
+            Arrays.sort(chArr);
+            String anagram = String.valueOf(chArr);
 
-                if(!map.containsKey(anagram)) {
-                    ArrayList<String> strList = new ArrayList<>();
-                    strList.add(str);
-                    map.put(anagram, strList);
-                }
-                else{
-                    ArrayList<String> strList = map.get(anagram);
-                    strList.add(str);
-                    map.put(anagram, strList);
-                }
+            if(!map.containsKey(anagram)) {
+                ArrayList<String> strList = new ArrayList<>();
+                strList.add(str);
+                map.put(anagram, strList);
             }
-            for(Map.Entry<String, ArrayList<String>> entry: map.entrySet()) {
-                list.add(entry.getValue());
+            else{
+                ArrayList<String> strList = map.get(anagram);
+                strList.add(str);
+                map.put(anagram, strList);
             }
+        }
+        for(Map.Entry<String, ArrayList<String>> entry: map.entrySet()) {
+            list.add(entry.getValue());
+        }
 
-            return list;
+        return list;
 
     }
 }
